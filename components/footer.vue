e96f2387ea3e9ac2bfa31ebba3e27dc6dcf0249a
Krypton <madhusudanbabar@gmail.com>
2020-06-14 12:57:21 +0530

    footer

--- components/footer.vue
+++ components/footer.vue
@@ -2,7 +2,7 @@
         <footer class="footer">
       <div class="footer__logo-box">
         <picture  class="footer__logo">
-            <img src="../assets/images/null.png" alt="">
+            <img src="../assets/images/null.svg" alt="">
           <!-- <source media="(max-width: 37.5em)" srcset="img/logo-green-small-1x.png 1x, img/logo-green-small-2x.png 2x"> -->
           <!-- <img srcset="img/logo-green-1x.png 1x, img/logo-green-2x.png 2x" src="img/logo-green-1x.png" alt="Natours"/> -->
         </picture>
@@ -32,6 +32,7 @@
         <div class="col-1-of-2">
           <p class="footer__copyright">
             null - The Open Security Community &copy; {{ new Date().getFullYear() }}
+
           </p>
         </div>
       </div>
