43e161319eb77bc2c9d62134a1c2080f91ef6ce1
Krypton <madhusudanbabar@gmail.com>
2020-06-22 14:39:30 +0530

    removed default navigation drawer

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,43 +1,5 @@
 <template>
   <v-app dark>
-    <v-navigation-drawer
-      absolute
-      dark
-      v-model="drawer"
-      :clipped="clipped"
-      app
-    >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-
-    <v-app-bar :clipped-left="clipped" fixed app>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-
-      <v-btn icon @click.stop="clipped = !clipped">
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-
-      <v-toolbar-title
-        class="mx-auto font-weight-bold display-1"
-        v-text="title"
-      />
-    </v-app-bar>
-
     <v-content>
       <v-container>
         <nuxt />
@@ -57,17 +19,6 @@ export default {
   },
   data() {
     return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      items: [
-        {
-          icon: "mdi-apps",
-          title: "Welcome",
-          to: "/"
-        }
-      ],
-      title: "null - Open Security Community "
     };
   }
 };
